d4a9780f672a2c4f55bb2041a26945f1fdec8719
채란 <nemo20435@gmail.com>
2023-04-18 19:55:38 +0900

    프로젝트 셋팅환경

---
+++ src/main/java/com/example/emart24/Emart24Application.java
@@ -0,0 +1,34 @@
+package com.example.emart24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class Emart24Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Emart24Application.class, args);
+  }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods(
+                HttpMethod.GET.name(),
+                HttpMethod.HEAD.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()
+            );
+      }
+    };
+  }
+}
---
+++ src/test/java/com/example/emart24/Emart24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emart24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Emart24ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
